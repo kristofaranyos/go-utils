48c1ade5d92a1107b156f46f7f14f9f2f855b444
kristofaranyos <kristof.aranyos@pento.io>
2022-11-20 11:42:27 +0100

    Added map contains methods

---
+++ maps/maps.go
@@ -0,0 +1,21 @@
+package maps
+
+func Contains[K comparable, V comparable](m map[K]V, v V) bool {
+	for _, e := range m {
+		if e == v {
+			return true
+		}
+	}
+
+	return false
+}
+
+func ContainsKey[K comparable, V comparable](m map[K]V, k K) bool {
+	for e, _ := range m {
+		if e == k {
+			return true
+		}
+	}
+
+	return false
+}
---
+++ maps/maps_test.go
@@ -0,0 +1,41 @@
+package maps_test
+
+import (
+	"testing"
+
+	"go-utils/maps"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestContains(t *testing.T) {
+	m := map[string]string{
+		"England": "London",
+		"Germany": "Berlin",
+		"France":  "Paris",
+	}
+
+	t.Run("contains Berlin", func(t *testing.T) {
+		assert.Equal(t, true, maps.Contains(m, "Berlin"))
+	})
+
+	t.Run("doesn't contain Brussels", func(t *testing.T) {
+		assert.Equal(t, false, maps.Contains(m, "Brussels"))
+	})
+}
+
+func TestContainsKey(t *testing.T) {
+	m := map[string]string{
+		"England": "London",
+		"Germany": "Berlin",
+		"France":  "Paris",
+	}
+
+	t.Run("contains Germany", func(t *testing.T) {
+		assert.Equal(t, true, maps.ContainsKey(m, "Germany"))
+	})
+
+	t.Run("doesn't contain Belgium", func(t *testing.T) {
+		assert.Equal(t, false, maps.ContainsKey(m, "Belgium"))
+	})
+}
